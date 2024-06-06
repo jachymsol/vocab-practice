4ed9b9faf629b01e92dcd9d776a8c56b0bfe5154
solejabreck@gmail.com <solejabreck@gmail.com>
2024-06-06 15:40:10 +0000

    Created HomePage.WordListForm.RowTemplate1; Edited HomePage.WordListForm; Deleted HomePage.WordListForm.ItemTemplate1

--- client_code/HomePage/WordListForm/RowTemplate1/__init__.py
+++ client_code/HomePage/WordListForm/RowTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,7 +7,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class ItemTemplate1(ItemTemplate1Template):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
