18687922f660bf14730d2b7399592321dfe23771
jachymsol <solejabreck@gmail.com>
2024-06-07 11:32:05 +0200

    Refactor Client Code

---
+++ client_code/HomePage/HomePage.py
@@ -0,0 +1,45 @@
+from ._anvil_designer import HomePageTemplate
+from PracticeForm import PracticeForm
+from WordListForm import WordListForm
+import anvil.server
+import anvil.users
+from anvil import Notification
+
+
+class HomePage(HomePageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.content_panel.add_component(PracticeForm())
+
+    # Any code you write here will run before the form opens.
+    if anvil.users.get_user():
+      self.show_logged_in()
+
+  def login_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.login_with_form(allow_cancel=True)
+    user = anvil.users.get_user()
+    if user:
+      if user['guid'] == None:
+        anvil.server.call('generate_guid', user.get_id())
+      self.show_logged_in()
+
+  def show_logged_in(self):
+    self.login_button.enabled = False
+    self.login_button.text = "Logged-In"
+    self.refresh_data_bindings()
+
+  def title_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    anvil.open_form('HomePage')
+
+  def home_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(PracticeForm())
+
+  def word_list_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(WordListForm())
--- client_code/HomePage/WordListForm/__init__.py
+++ client_code/HomePage/WordListForm/__init__.py
@@ -1,13 +1,1 @@
-from ._anvil_designer import WordListFormTemplate
-from anvil import Button
-import anvil.server
-
-
-class WordListForm(WordListFormTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
-    words_list = anvil.server.call('get_words_list')
-    self.word_row.items = [{'word': word['word'], 'n_practiced': word['n_practiced'], 'learned': word['learned'], 'confidence': word['guid']} for word in words_list]
+from WordListForm import WordListForm
