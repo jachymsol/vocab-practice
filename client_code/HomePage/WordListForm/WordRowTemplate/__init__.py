6fa4405ac3bb14843b7e53e69f02eae91a4196dd
solejabreck@gmail.com <solejabreck@gmail.com>
2024-06-07 08:02:37 +0000

    Added WordRow

--- client_code/HomePage/WordListForm/WordRowTemplate/__init__.py
+++ client_code/HomePage/WordListForm/WordRowTemplate/__init__.py
@@ -13,3 +13,10 @@ class WordRowTemplate(WordRowTemplateTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def remove_word_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if confirm(f"Do you wish to delete word {self.item['word']}?"):
+      anvil.server.call('delte_word', self.item['word'])
+      anvil.Notification(f"Word {self.item['word']} deleted successfully").show()
+      self.remove_from_parent()
